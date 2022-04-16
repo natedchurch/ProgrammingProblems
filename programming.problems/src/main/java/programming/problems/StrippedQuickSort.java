bc679d23d6bc1374f017a0e382f5f40ec918b20e
Nate Church <nchurch@us.ibm.com>
2022-04-15 17:28:20 -0700

    Share project.

---
+++ programming.problems/src/main/java/programming/problems/StrippedQuickSort.java
@@ -0,0 +1,30 @@
+package programming.problems;
+
+
+public class StrippedQuickSort {
+
+	public static void main(String[] args) {
+		
+		int[] numbers = {3, 7, 0, 2, 4, 8, 9, 5, 1, 6};
+
+		quicksort(numbers);
+	}
+	
+	public static void quicksort(int[] numbers) {
+		quicksort(numbers, 0, numbers.length - 1, 0);
+	}
+	
+	public static void quicksort(int[] numbers, int leftMostIx, int rightMostIx, int depth) {
+		int pivotIx = rightMostIx;
+		int currLeft = leftMostIx;
+		int currRight = rightMostIx - 1;
+		
+		//TODO add code here
+		
+	}
+	
+	private static void swap(int[] numbers, int leftIx, int rightIx) {
+		//TODO add code here
+	}
+	
+}
